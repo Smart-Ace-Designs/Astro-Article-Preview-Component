0902c7feb02f84146d1d53b7af4d51c5b0a0f7ee
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-01-28 23:41:22 -0600

    Converts custom CSS to kabab-case for Tailwind 4 upgrade

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        "sad-very-dark-grayish-blue": "hsl(217, 19%, 35%)",
-        "sad-desaturated-dark-blue": "hsl(214, 17%, 51%)",
-        "sad-grayish-blue": "hsl(212, 23%, 69%)",
-        "sad-light-grayish-blue": "hsl(210, 46%, 95%)",
+        "theme-very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "theme-desaturated-dark-blue": "hsl(214, 17%, 51%)",
+        "theme-grayish-blue": "hsl(212, 23%, 69%)",
+        "theme-light-grayish-blue": "hsl(210, 46%, 95%)",
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
