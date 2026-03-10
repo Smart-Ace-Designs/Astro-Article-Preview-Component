2515fde2f30038927e546211574c1a6f4d084540
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-10 09:53:59 -0500

    Remove experimental flag for Fonts API

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Manrope",
-        cssVariable: "--font-manrope",
-        weights: [400, 700],
-        styles: ["normal"],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Manrope",
+      cssVariable: "--font-manrope",
+      weights: [400, 700],
+      styles: ["normal"],
+    },
+  ],
 });
